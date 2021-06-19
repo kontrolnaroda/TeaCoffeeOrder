463cf58fc27f5a39764309a39155816f4becee76
Yuri Pavlov <kontrolnaroda@mail.ru>
2021-06-19 17:38:09 +0300

    Aleksandr building2

---
+++ app/src/main/java/com/example/teacoffeeorder/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.teacoffeeorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doOrder(View view) {
+        Intent intent = new Intent(this,OrderActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/teacoffeeorder/OrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.teacoffeeorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
