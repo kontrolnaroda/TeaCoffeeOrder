5155f1fbb592923446b5c878036169ba9844a1e0
Yuri Pavlov <kontrolnaroda@mail.ru>
2021-07-11 00:58:39 +0300

    my first Toast

--- app/src/main/java/com/example/teacoffeeorder/LoginActivity.java
+++ app/src/main/java/com/example/teacoffeeorder/LoginActivity.java
@@ -4,18 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+private EditText editTextPersonName;
+private EditText editTextPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextPersonName = findViewById(R.id.editTextTextPersonName);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+
     }
 
     public void doOrder(View view) {
-        Intent intent = new Intent(this,OrderActivity.class);
-        startActivity(intent);
-    }
-}
+        String name = editTextPersonName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if (!name.isEmpty() && !password.isEmpty()) {
+
+            Intent intent = new Intent(this, OrderActivity.class);
+            startActivity(intent);
+        }else{
+Toast toast = Toast.makeText(LoginActivity.this,"Все поля должны быть заполнены!", Toast.LENGTH_SHORT);
+
+            toast.show();
+        }
+}}
